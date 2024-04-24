96149003c8cc4a75ce1d8fdb6499bb8a02e3c592
Martins Z <112480628+Zuivids@users.noreply.github.com>
2024-04-24 16:48:19 +0300

    added validation

--- cypress/e2e/demoqa.cy.js
+++ cypress/e2e/demoqa.cy.js
@@ -2,15 +2,19 @@ import { AutomationPracticeForm } from "../pageObjects/AutomationPracticeForm.pa
 
 describe("Final Task in Web_Aut", () => {
     context("Input all the necessary information", () => {
-        it('PracticeForm', () => {
+        it('PracticeFormTest', () => {
             // a. Open https://demoqa.com/automation-practice-form
             AutomationPracticeForm.visit();
             // b. Input all the necessary information in the text fields, buttons, etc, where it is not stated otherwise.
-            AutomationPracticeForm.enterFirstName.type("Mārtiņš");
-            AutomationPracticeForm.enterLastName.type("Žaimis");
-            AutomationPracticeForm.enterEmail.type("aizmirsu@kautkad.lv")
+            var name = "Mārtiņš";
+            var surname = "Žaimis";
+            var email = "aizmirsu@kautkad.lv"
+            var mobile = "1234567891";
+            AutomationPracticeForm.enterFirstName.type(name);
+            AutomationPracticeForm.enterLastName.type(surname);
+            AutomationPracticeForm.enterEmail.type(email)
             AutomationPracticeForm.checkedRadioButton("Male").check({force: true});
-            AutomationPracticeForm.enterMobileNumber.type("1234567891");
+            AutomationPracticeForm.enterMobileNumber.type(mobile);
             // c. Set the - Date of Birth - with Calendar widget to - 28th of February, 1930.
             AutomationPracticeForm.enterDateOfBirth.type("{selectall}28 Feb 1930{enter}");
             // d. Set Subjects to Economics.
@@ -21,17 +25,17 @@ describe("Final Task in Web_Aut", () => {
             // i. Upload an image of your choice.
             AutomationPracticeForm.uploadImage;
             // g. Set State to NCR.
-            AutomationPracticeForm.enterAdress.type("Kaut kada adrese 1");
+            var adress = "Kaut kada adrese 1";
+            AutomationPracticeForm.enterAdress.type(adress);
             AutomationPracticeForm.setState.type("NCR{enter}");
             // h. Set City to Delhi.
             AutomationPracticeForm.setCity.type("Delhi{enter}");
             // i. Click Submit.
             AutomationPracticeForm.submit.click();
             //j. Validation
-            //TODO
-
-
+           AutomationPracticeForm.confirmationText.should("have.text","Thanks for submitting the form");
+           AutomationPracticeForm.validateTable;
+           
         })
-
     });
 });
--- cypress/pageObjects/AutomationPracticeForm.page.js
+++ cypress/pageObjects/AutomationPracticeForm.page.js
@@ -55,4 +55,30 @@ export class AutomationPracticeForm{
     static get submit(){
         return cy.get("#submit");
     }
+
+    static get validateStudentName(){
+        return cy.get("");
+    }
+
+    static get confirmationText(){
+        return cy.get("#example-modal-sizes-title-lg");
+    }
+
+    static get validateTable(){
+        cy.get('tbody').within(() => {
+            // avots  =>https://www.youtube.com/watch?v=M0RxEeJaq_E
+            cy.contains('tr', 'Student Name').should('contain', "Mārtiņš Žaimis");
+            cy.contains('tr', 'Student Email').should('contain', 'aizmirsu@kautkad.lv');
+            cy.contains('tr', 'Gender').should('contain', 'Male');
+            cy.contains('tr', 'Mobile').should('contain', '1234567891');
+            cy.contains('tr', 'Date of Birth').should('contain', '28 February,1930');
+            cy.contains('tr', 'Subjects').should('contain', 'Economics');
+            cy.contains('tr', 'Hobbies').should('contain', 'Music');
+            //cy.wait(500);
+            cy.contains('tr', 'Picture').should('contain', 'test_image.png');
+            cy.contains('tr', 'Address').should('contain', 'Kaut kada adrese 1'); 
+            cy.contains('tr', 'State and City').should('contain', 'NCR Delhi');
+        
+        });
+     }
 }
